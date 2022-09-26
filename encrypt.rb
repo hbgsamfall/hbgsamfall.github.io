b4a234f6a675fb9b3a44d5eab989cb12660f2ae9
hbg <hbg@hbg>
2022-09-26 22:50:39 +0200

    Add secret files

--- encrypt.rb
+++ encrypt.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/ruby
 readData = File.open("hemligt/losenord.txt")
-pwd = readData.read
+pwd = readData.read.delete!("\r\n\\")
 
 Dir.glob('hemligt/**/*')
   .select { |e| File.file? e }
   .select { |e| !e.end_with?("txt")  }
-  .each{ |e| system("echo " +e) }
+  .each{ |e|  puts( "7z a -mx=0 -p"+pwd+" \""+e.sub("hemligt","krypterat")+".7z\" \"./"+e+"\"") }
 
-#7z e foo.7z <file>
+#7z a -mx=0 -pXXX "krypterat/2015.pdf.7z" "./hemligt/2015.pdf"
+
 
